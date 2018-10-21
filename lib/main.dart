ab23ad4f9e2cd76307b953731501bb70d2beeec8
ameyak11 <amukulkarni11@gmail.com>
2018-10-21 13:16:40 +0530

    added icon theme to make send button blue

--- lib/main.dart
+++ lib/main.dart
@@ -31,25 +31,28 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
-  return new Container(
-    margin: const EdgeInsets.symmetric(horizontal: 8.0),
-    child: Row(
-      children: <Widget>[
-        Flexible(
-            child: new TextField(
-          controller: _textController,
-          onSubmitted: _handleSubmitted,
-          decoration: new InputDecoration.collapsed(
-            hintText: "Send a message"),
-        ),
-        ),
-        Container(                                                  
-          margin: new EdgeInsets.symmetric(horizontal: 4.0),            
-          child: new IconButton(                                        
-            icon: new Icon(Icons.send),                                 
-            onPressed: () => _handleSubmitted(_textController.text)),   
-        ),
-        ]
+  return IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor), 
+      child: new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+              child: new TextField(
+            controller: _textController,
+            onSubmitted: _handleSubmitted,
+            decoration: new InputDecoration.collapsed(
+              hintText: "Send a message"),
+          ),
+          ),
+          Container(                                                  
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),            
+            child: new IconButton(                                        
+              icon: new Icon(Icons.send),                                 
+              onPressed: () => _handleSubmitted(_textController.text)),   
+          ),
+          ]
+      ),
     ),
   );
 }
